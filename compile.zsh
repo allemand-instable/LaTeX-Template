928dd072f09726f24f1a863a9a5130e650ac055f
allemand-instable <allemandinstable@gmail.com>
2023-10-18 00:03:46 +0200

    massive bug fixes

--- compile.zsh
+++ compile.zsh
@@ -5,4 +5,27 @@ if (!-x $evince_synctex_path) {
     chmod 0755, $evince_synctex_path;
 }
 
+if [ ! -d "./aux_files" ]; then
+    echo "directory \"./aux_files\" does not exist"
+    echo "creating directory \"./aux_files\""
+    mkdir -p "./aux_files"
+fi
+
+
+# ⚠️ | fixing the error : I CANT WRITE TO FILE
+# $  | when the include is in a subfolder
+content_list=(src/content/*)
+# removes "src/"
+content_list_trimmed=("${content_list[@]/#src\//}")
+
+for item in "${content_list_trimmed[@]}"; do
+    echo "checking if directory $item is available in aux_files..."
+    if [ ! -d "aux_files/$item" ]; then
+        echo "creating directory $item in aux_files..."
+        mkdir -p "aux_files/$item"
+    else
+        echo "directory $item already exists in aux_files..."
+    fi
+done
+
 latexmk -pdf -bibtex -pv -time
